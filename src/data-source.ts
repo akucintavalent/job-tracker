932b6bb0c67790f5b4103003d2279f262cd5e722
Bohdan Shcherbak <bogdan@desmart.com>
2024-01-10 21:20:18 +0100

    chore: remove redundant console.log

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,6 @@
 import { DataSource } from 'typeorm';
 import { getDataSourceOptions } from '../database.config';
 
-console.log(getDataSourceOptions());
-
 export const AppDataSource = new DataSource(getDataSourceOptions());
 
 AppDataSource.initialize()
