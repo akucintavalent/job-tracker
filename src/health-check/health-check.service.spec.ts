eb0e6fa952c4bcf9c38324740be680c887ba6f96
Bohdan Shcherbak <bogdan@desmart.com>
2024-01-10 21:50:38 +0100

    fix: tests in healthcheck module

--- src/health-check/health-check.service.spec.ts
+++ src/health-check/health-check.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthCheckService } from './health-check.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from '../data-source';
 
 describe('HealthCheckService', () => {
   let service: HealthCheckService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          ...AppDataSource.options,
+        }),
+      ],
       providers: [HealthCheckService],
     }).compile();
 
