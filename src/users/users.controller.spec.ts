31f0812d257117aaf26f1c1d1d2933d25ced6820
Bohdan Shcherbak <bogdan@desmart.com>
2024-01-11 17:29:19 +0100

    fix: tests for usersuse real db for tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDataSourceOptions } from '../../database.config';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          ...getDataSourceOptions(),
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
+      providers: [UsersService],
       controllers: [UsersController],
     }).compile();
 
