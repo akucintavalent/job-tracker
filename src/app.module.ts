cc0f8d1af3c5c424ca7fbff26797d49bb9eb9627
Bohdan Shcherbak <bogdan@desmart.com>
2024-01-11 14:38:36 +0100

    chore: add validation pipe to be able to use dto decorator for validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './data-source';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { UsersModule } from './users/users.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -15,6 +16,14 @@ import { UsersModule } from './users/users.module';
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
 export class AppModule {}
