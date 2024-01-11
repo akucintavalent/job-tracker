44da103d1a2e0104645d31115e28d33000d9afa4
Bohdan Shcherbak <bogdan@desmart.com>
2024-01-11 17:24:38 +0100

    chore: don't import AppDataSource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,16 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppDataSource } from './data-source';
+// import { AppDataSource } from './data-source';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
+import { getDataSourceOptions } from '../database.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      ...AppDataSource.options,
+      ...getDataSourceOptions(),
     }),
     HealthCheckModule,
     UsersModule,
