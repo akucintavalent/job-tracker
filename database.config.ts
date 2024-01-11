80c05241241a0f8ccc46529b7a1dbb6768e36432
Bohdan Shcherbak <bogdan@desmart.com>
2024-01-11 17:21:42 +0100

    feat: use psql even during tests but from different db

--- database.config.ts
+++ database.config.ts
@@ -8,23 +8,17 @@ if (process.env.NODE_ENV !== 'test') {
   config({ path: '.env.test' });
 }
 
-export const getDataSourceOptions = (): DataSourceOptions =>
-  process.env.NODE_ENV === 'test'
-    ? {
-        type: 'sqlite',
-        database: process.env.DB_NAME,
-        entities: [User],
-        synchronize: true,
-      }
-    : {
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: [User],
-        synchronize: true,
-      };
+export const getDataSourceOptions = (): DataSourceOptions => ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [User],
+  migrations: ['src/migrations/*.js'],
+  migrationsRun: true,
+  synchronize: false,
+});
 
 export default getDataSourceOptions;
