1117321d3a8665784a89826fbfee0a3b523917cd
Bohdan Shcherbak <b.szczerbak@gmail.com>
2024-01-13 14:16:54 +0100

    feat: adding swagger decorators (wip)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,10 +12,17 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { FindUsersDto } from './dtos/find-users.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 
 @ApiTags('users')
 @Controller('users')
@@ -25,21 +32,42 @@ export class UsersController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new user' })
-  @ApiResponse({ status: 200, description: 'User record', type: CreateUserDto })
+  @ApiCreatedResponse({ description: 'User record', type: UserDto })
   @ApiResponse({
     status: 400,
-    description: 'Validation error or user is already exists',
+    description: 'Validation error or user already exists',
   })
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Fetch all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'User records',
+    type: Array<UserDto>,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Validation error',
+  })
   findUsers(@Query() query: FindUsersDto) {
     return this.usersService.findBy(query);
   }
 
   @Get('/:id')
+  @ApiOperation({ summary: 'Fetch user' })
+  @ApiResponse({
+    status: 200,
+    description: 'User record',
+    type: UserDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Validation error',
+  })
+  @ApiNotFoundResponse({ description: 'User not found' })
   findUserById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.findOneBy({ id });
   }
