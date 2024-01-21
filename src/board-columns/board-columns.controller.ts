5445b1908f3ef6f54113a8bd3e8a74856a19b421
Oleksandr Oliinyk <vov96now@gmail.com>
2024-01-21 11:41:00 +0200

    Minimize board-columns.controller.ts

--- src/board-columns/board-columns.controller.ts
+++ src/board-columns/board-columns.controller.ts
@@ -21,18 +21,9 @@ export class BoardColumnsController {
 
   @Post()
   @ApiOperation({ summary: 'Creates a new column for a board' })
-  @ApiResponse({
-    status: 200,
-    description: 'Column created',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Board not found',
-  })
+  @ApiResponse({ status: 200, description: 'Column created' })
+  @ApiResponse({ status: 400, description: 'Validation error' })
+  @ApiResponse({ status: 400, description: 'Board not found' })
   async createBoard(@Body() boardDto: CreateBoardColumnDto) {
     return await this.boardColumnsService.create(boardDto);
   }
@@ -40,14 +31,8 @@ export class BoardColumnsController {
   @Get('/:id')
   @ApiParam({ name: 'id', description: 'Board id' })
   @ApiOperation({ summary: `Fetch all board's column` })
-  @ApiResponse({
-    status: 200,
-    description: 'Column records',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
+  @ApiResponse({ status: 200, description: 'Column records' })
+  @ApiResponse({ status: 400, description: 'Validation error' })
   findColumns(@Param('id', ParseUUIDPipe) boardId: string) {
     return this.boardColumnsService.findColumns(boardId);
   }
@@ -59,14 +44,8 @@ export class BoardColumnsController {
     type: [String],
   })
   @ApiOperation({ summary: `Rearange all columns for this Board` })
-  @ApiResponse({
-    status: 200,
-    description: 'Columns rearanged',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
+  @ApiResponse({ status: 200, description: 'Columns rearanged' })
+  @ApiResponse({ status: 400, description: 'Validation error' })
   async rearange(@Param('id', ParseUUIDPipe) boardId: string, @Body() columnIds: string[]) {
     await this.boardColumnsService.rearangeColumns(boardId, columnIds);
   }
@@ -74,18 +53,9 @@ export class BoardColumnsController {
   @Patch('/:id')
   @ApiParam({ name: 'id', description: 'Column id' })
   @ApiOperation({ summary: `Update column` })
-  @ApiResponse({
-    status: 200,
-    description: 'Column updated',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Column not found',
-  })
+  @ApiResponse({ status: 200, description: 'Column updated' })
+  @ApiResponse({ status: 400, description: 'Validation error' })
+  @ApiResponse({ status: 400, description: 'Column not found' })
   async updateBoard(
     @Param('id', ParseUUIDPipe) columnId: string,
     @Body() body: UpdateBoardColumnDto,
@@ -96,14 +66,8 @@ export class BoardColumnsController {
   @Delete('/:id')
   @ApiParam({ name: 'id', description: 'Column id' })
   @ApiOperation({ summary: 'Delete column' })
-  @ApiResponse({
-    status: 200,
-    description: 'Column deleted',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
+  @ApiResponse({ status: 200, description: 'Column deleted' })
+  @ApiResponse({ status: 400, description: 'Validation error' })
   deleteBoard(@Param('id', ParseUUIDPipe) columnId: string) {
     return this.boardColumnsService.remove(columnId);
   }
