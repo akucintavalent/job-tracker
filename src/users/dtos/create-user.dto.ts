4764f0fda00dead65c00c5a3eef2d4a5ab2849da
Oleksandr Oliinyk <vov96now@gmail.com>
2024-01-24 10:01:51 +0200

    Update CreateUserDto

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,19 +1,24 @@
 import { IsEmail, IsEnum, IsString, Matches } from 'class-validator';
 import { UserRole, UserRoleType } from '../enums/user-role.enum';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 
+  @ApiProperty()
   @Matches(/^[a-zA-Z0-9_]{4,16}$/, {
     message: 'Username is not valid.',
   })
   @IsString()
   username: string;
 
+  @ApiPropertyOptional()
   @IsEnum(UserRole)
   role?: UserRoleType;
 }
