f02b8c7a4c4aad9ac255130bde14719811461ead
Oleksandr Oliinyk <vov96now@gmail.com>
2024-05-13 10:06:15 +0300

    86by0fn2r Implement sendEmail()

--- src/email-sender/email-sender.service.ts
+++ src/email-sender/email-sender.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Resend } from 'resend';
 
 @Injectable()
-export class EmailSenderService {}
+export class EmailSenderService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async sendVerificationEmail(to: string) {
+    const code = '1234';
+    await this.sendEmail('SignUp', to, 'JobTracker Email verification', `Code: ${code}`);
+  }
+
+  private async sendEmail(from: string, to: string, subject: string, body: string) {
+    const token = this.configService.get('RESEND_TOKEN');
+    const emailFrom = this.configService.get('NOTIFICATION_EMAIL');
+    const resend = new Resend(token);
+    const { data, error } = await resend.emails.send({
+      from: `${from} <${emailFrom}>`,
+      to: [to],
+      subject: subject,
+      html: body,
+    });
+
+    if (error) {
+      return console.error('Error during sending email', { error });
+    }
+
+    console.log('Email sent', { data });
+  }
+}
