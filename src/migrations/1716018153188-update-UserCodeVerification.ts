ada1e77fbbacb01e3c3b90834c7b5186c0bfddbc
Oleksandr Oliinyk <vov96now@gmail.com>
2024-05-18 11:55:27 +0300

    86by0fn2r Fix migration issue

--- src/migrations/1716018153188-update-UserCodeVerification.ts
+++ src/migrations/1716018153188-update-UserCodeVerification.ts
@@ -7,13 +7,9 @@ export class UpdateUserCodeVerification1716018153188 implements MigrationInterfa
     await queryRunner.query(
       `ALTER TABLE "user-code-verifications" ADD "code" character varying(6) NOT NULL`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "users" ADD "is-email-verified" boolean NOT NULL DEFAULT false`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "is-email-verified"`);
     await queryRunner.query(`ALTER TABLE "user-code-verifications" DROP COLUMN "code"`);
   }
 }
