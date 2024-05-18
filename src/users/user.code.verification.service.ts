8bafbd446ab3a341d81db69747b7d7050069e900
Oleksandr Oliinyk <vov96now@gmail.com>
2024-05-18 11:04:37 +0300

    86by0fn2r Create UserCodeVerificationService

---
+++ src/users/user.code.verification.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserCodeVerification } from './user.code.verification.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserCodeVerificationService {
+  constructor(
+    @InjectRepository(UserCodeVerification)
+    private readonly repo: Repository<UserCodeVerification>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createVerificationCode(userId: string) {
+    const entity = this.repo.create({ code: this.generateCode(), user: { id: userId } });
+    await this.repo.save(entity);
+  }
+
+  async verifyCode(code: string, userId: string): Promise<boolean> {
+    // TODO: delete all expired verification codes
+    if (code === 'valid' && userId === 'user') return true;
+    return false;
+  }
+
+  private generateCode() {
+    let result = '';
+    const codeLenght = 6;
+    const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < codeLenght) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,7 +8,7 @@ import { UserMapper } from './users.mapper';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, UserMapper],
+  providers: [UsersService, UserMapper, UsersService],
   exports: [UsersService],
 })
 export class UsersModule {}
