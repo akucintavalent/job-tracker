1af06dbb08becada7de178e5f8fc10e23bc33b88
Oleksandr Oliinyk <vov96now@gmail.com>
2024-06-15 09:51:02 +0300

    Rename `job_applications` table

--- src/job-applications/entities/job-application.entity.ts
+++ src/job-applications/entities/job-application.entity.ts
@@ -3,7 +3,7 @@ import { BoardColumn } from '../../board-columns/entities/board-column.entity';
 import { Column, Entity, JoinColumn, ManyToMany, ManyToOne } from 'typeorm';
 import { Contact } from '../../contacts/entities/contact.entity';
 
-@Entity('job-applications')
+@Entity('job_applications')
 export class JobApplication extends BaseEntity {
   @Column()
   title: string;
