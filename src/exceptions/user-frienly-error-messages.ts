08ae7bc94037ccc673c0752703e4a15e5038e3a3
Oleksandr Oliynyk <vov96now@gmail.com>
2024-06-20 20:41:37 +0300

    Update error messages

--- src/exceptions/user-frienly-error-messages.ts
+++ src/exceptions/user-frienly-error-messages.ts
@@ -1,7 +1,7 @@
 export enum UserFriendlyErrorMessages {
   // Email code verification
-  EMAIL_CODE_NOT_FOUND = 'Verification code has not been found.',
-  EMAIL_CODE_EXPIRED = 'Verification code has been expired.',
+  EMAIL_CODE_NOT_FOUND = 'The verification code has not been found.',
+  EMAIL_CODE_EXPIRED = 'The verification code has expired. Please send the verification code again.',
   // Login
-  EMAIL_NOT_VERIFIED = 'Email is not verified. Verify email first before continue.',
+  EMAIL_NOT_VERIFIED = 'Email not verified. Please verify your email address before continuing.',
 }
