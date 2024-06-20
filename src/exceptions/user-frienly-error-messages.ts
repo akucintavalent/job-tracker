8cd0801b26e63151c41a7b5620d571c9a3530133
Oleksandr Oliynyk <vov96now@gmail.com>
2024-06-20 21:21:10 +0300

    Fix after code review

--- src/exceptions/user-frienly-error-messages.ts
+++ src/exceptions/user-frienly-error-messages.ts
@@ -1,7 +1,7 @@
 export enum UserFriendlyErrorMessages {
   // Email code verification
   EMAIL_CODE_NOT_FOUND = 'The verification code has not been found.',
-  EMAIL_CODE_EXPIRED = 'The verification code has expired. Please send the verification code again.',
+  EMAIL_CODE_EXPIRED = 'The verification code has expired. Please request a new one.',
   // Login
   EMAIL_NOT_VERIFIED = 'Email not verified. Please verify your email address before continuing.',
 }
