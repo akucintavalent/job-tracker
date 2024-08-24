05bb027c41a16cb78c4524c466b6e66074d6bef1
Oleksandr Oliinyk <vov96now@gmail.com>
2024-08-24 08:51:47 +0300

    Fix unit test

--- src/modules/job-application-notes/dtos/job-application-note.dto.ts
+++ src/modules/job-application-notes/dtos/job-application-note.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, IsUUID } from 'class-validator';
-import { BaseDto } from 'src/dtos/base.dto';
+import { BaseDto } from '../../../dtos/base.dto';
 
 export class JobApplicationNoteDto extends BaseDto {
   @ApiProperty({ type: 'string', format: 'uuid' })
