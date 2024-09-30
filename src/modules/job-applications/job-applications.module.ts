065a93b7f8aa3c1851fd93a94ca86f36f8a80bad
Bohdan Shcherbak <b.szczerbak@gmail.com>
2024-09-30 21:15:41 +0200

    Import necessary repositories

--- src/modules/job-applications/job-applications.module.ts
+++ src/modules/job-applications/job-applications.module.ts
@@ -6,9 +6,13 @@ import { JobApplication } from './entities/job-application.entity';
 import { BoardColumn } from '../board-columns/entities/board-column.entity';
 import { JobApplicationMapper } from './job-applications.mapper';
 import { JobApplicationNote } from '../job-application-notes/entities/job-application-note.entity';
+import { Contact } from '../contacts/entities/contact.entity';
+import { Company } from '../companies/entities/company.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([JobApplication, JobApplicationNote, BoardColumn])],
+  imports: [
+    TypeOrmModule.forFeature([JobApplication, JobApplicationNote, BoardColumn, Contact, Company]),
+  ],
   controllers: [JobApplicationsController],
   providers: [JobApplicationsService, JobApplicationMapper],
 })
